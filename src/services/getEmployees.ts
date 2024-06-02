d44fa6ba69b63909e3d47507bf29be4dac354879
Elsa Eriksson <elsa.95.eriksson@gmail.com>
2024-06-02 18:45:13 +0200

    url change

--- src/services/getEmployees.ts
+++ src/services/getEmployees.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { Employees } from "@/types/Employees";
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VUE_APP_API_URL;
 
 export const getEmployees = async (
   page: number
